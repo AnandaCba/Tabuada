1584562252f9393a9ca387351c58ed06123d406d
anandacba <fabiogurudeva@gmail.com>
2021-05-15 13:16:10 -0300

    Refactoring code

--- script.js
+++ script.js
@@ -1,34 +1,32 @@
-txtn.focus()
-function tabuada(){
-// Declarando as variaveis
-let numero = document.getElementById('txtn')
-let tab = document.getElementById('tabu')
-    //Nesse caso usar como teste o valor inserido pelo user.
-    //Não o convertido para number
-    //Se o tamanho do var da VAR for igual a 0 ou "Nada"
-    if(numero.value.length == 0){
-        alert('Favor insira um valor!') 
-        // ELSE vai impedir que o código continue 
-    } else {
-        //Transforma a VAR numero em Number 
-        let num = Number(numero.value)
-        //Limpar 
-        tab.innerHTML = ""
-        //FOR para criar de 1 a 10 da tabuada
-        for(let c = 1; c <= 10; c++){
-        //Criado um select em HTML
-        //Criando option em JS de forma dinâmica    
-        let item = document.createElement('option')
-        //Exibir para o user
-        item.text = `${num}x${c} = ${num*c}`
-        //item.value = `tab${c}`
-        //Inserindo no HTML
-        tab.appendChild(item)
-        //Limpar 
-        document.getElementById("txtn").value = ""
-        //inicirar cursor no input
-        txtn.focus()
-        }
-    } 
-}
 
+function timesTables(){
+
+    let multiplierNumberStr = document.getElementById('inputUserNumber')
+    let table = document.getElementById('tabu')
+    
+    //Validação
+    if(multiplierNumberStr.value.length == 0){
+        alert('Favor insira um valor!') 
+    } else { 
+        let multiplierNumber = Number(multiplierNumberStr.value)
+        //Limpa 
+        table.innerHTML = ""
+
+        //Criar de 1 a 10 da tabuada
+        for(let count = 1; count <= 10; count++){
+            //Criando option em JS de forma dinâmica    
+            let item = document.createElement('option')      
+            item.text = `${multiplierNumber}x${count} = ${multiplierNumber * count}`
+            //Inserindo no HTML
+            table.appendChild(item)
+            inputUserNumber.focus()
+        };
+        //Limpar 
+        document.getElementById("inputUserNumber").value = ""
+    }; 
+};
+
+//Foco ao abrir a pagina
+window.onload = function() {
+    document.getElementById("inputUserNumber").focus();
+};
